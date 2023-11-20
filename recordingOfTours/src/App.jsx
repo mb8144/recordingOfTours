a79b35ae849e3ce9a44e06c1b3f58c5d0a89b8ed
mb8144 <maurusbrunnschweiler@gmail.com>
2023-11-20 21:45:28 +0100

    first steps

--- recordingOfTours/src/App.jsx
+++ recordingOfTours/src/App.jsx
@@ -1,16 +1,15 @@
-import './App.css'
-import Navbar from './Navbar'
-import Home from './Home'
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-
-
   return (
-    <>
-     <Navbar />
-     <Home />
-    </>
-  )
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- recordingOfTours/src/Home.jsx
+++ recordingOfTours/src/Home.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+
 const Home = () => {
-  return (  
-    <div>This is the Launch page</div>
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
+
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
   );
 }
  
