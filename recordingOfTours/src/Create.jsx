233717138450fc1a7bd245635f37e60aff9f0af0
mb8144 <maurusbrunnschweiler@gmail.com>
2023-11-22 22:24:53 +0100

    form ready

--- recordingOfTours/src/Create.jsx
+++ recordingOfTours/src/Create.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [region, setRegion] = useState('empty');
   return ( 
     <div className="create">
       <h2>Add a new Tour</h2>
+      <form>
+        <label>Tour title: </label>
+          <input 
+            type="text"
+            required
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        <label>Your Description (still body): </label>
+          <textarea
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          />
+        <label>Region: </label>
+        <select
+          value={region}
+          onChange={(e) => setRegion(e.target.value)}
+        >
+          <option value="empty"></option>
+          <option value="Graubünden">Graubünden</option>
+          <option value="Wallis">Wallis</option>
+          <option value="Zentralschweiz">Zentralschweiz</option>
+          <option value="Tessin">Tessin</option>
+        </select>
+        <button>Add Tour</button>
+      </form>
     </div>
    );
 }
