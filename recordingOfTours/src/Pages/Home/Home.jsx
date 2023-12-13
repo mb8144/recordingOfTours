47e62793d621100db51d700d090965cc050990e1
mb8144 <maurusbrunnschweiler@gmail.com>
2023-12-13 18:02:41 +0100

    ready for tailwind integration

--- recordingOfTours/src/Pages/Home/Home.jsx
+++ recordingOfTours/src/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 const Home = () => {
   return ( 
     <>
-    <div class="animated-element"></div>
+    <div className="animated-element"></div>
     <div className="home-text">
       <h2>Welcome!</h2>
       <p className="tool-questions">Do you ever think of which tours you already did in the mountains? Thought about them as inspiration for your next adventure? Searched for something to recommend to your friend? Or even had to prove them for a course or expedition?</p>
       <p>Here's where TourRecord comes into place. It's a tool created for you to track your tours for various reasons to solve well-known problems.</p>
+    </div>
+    <div>
+      This whole Home is created with the help of Tailwind css.
     </div>
     <h2>Welcome to Tour Record</h2>
       <h4>Ideas for this home component. Most probably no scrolling needed</h4>
